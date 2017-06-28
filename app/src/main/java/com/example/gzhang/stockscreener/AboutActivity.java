b1df79d7863b457bb32ea34db831463bbaf3b59f
GarlandZhang <garland._zhang@hotmail.com>
2017-06-27 20:00:43 -0400

    updated version

--- app/src/main/java/com/example/gzhang/stockscreener/AboutActivity.java
+++ app/src/main/java/com/example/gzhang/stockscreener/AboutActivity.java
@@ -1,9 +1,22 @@
 package com.example.gzhang.stockscreener;
 
 import android.app.Activity;
+import android.os.Bundle;
+import android.view.View;
 
 /**
  * Created by GZhang on 2017-06-24.
  */
 public class AboutActivity extends Activity{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.about_layout);
+
+    }
+
+    public void onBackPress(View view) {
+        finish();
+    }
 }
--- app/src/main/java/com/example/gzhang/stockscreener/ScreenerActivity.java
+++ app/src/main/java/com/example/gzhang/stockscreener/ScreenerActivity.java
@@ -1,11 +1,24 @@
 package com.example.gzhang.stockscreener;
 
 import android.app.Activity;
+import android.os.Bundle;
+import android.view.View;
 
 /**
  * Created by GZhang on 2017-06-05.
  */
 public class ScreenerActivity extends Activity{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.screener_layout);
+
+    }
+
+    public void onBackPress(View view) {
+        finish();
+    }
 
     //TODO: everything.
     /*List of ideas
