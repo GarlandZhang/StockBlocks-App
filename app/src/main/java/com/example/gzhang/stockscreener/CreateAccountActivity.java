c8ae4e79580b48d3fe8e35f36d231496c84205d0
Leon Dai <linchuandai@gmail.com>
2017-07-02 13:13:35 -0400

    Add files via upload

---
+++ app/src/main/java/com/example/gzhang/stockscreener/CreateAccountActivity.java
@@ -0,0 +1,49 @@
+package com.example.gzhang.stockscreener;
+
+import android.app.Activity;
+
+import android.content.Intent;
+import android.graphics.BitmapFactory;
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+import android.widget.CheckBox;
+
+/*
+import com.amazonaws.ClientConfiguration;
+import com.amazonaws.auth.CognitoCachingCredentialsProvider;
+import com.amazonaws.regions.Region;
+import com.amazonaws.regions.Regions;
+import com.amazonaws.services.dynamodbv2.*;
+import com.amazonaws.mobileconnectors.dynamodbv2.dynamodbmapper.*;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Scanner;
+*/
+
+
+/**
+ * Created by leon_ on 2017-06-30.
+ */
+
+public class CreateAccountActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.createaccount_layout);
+
+
+    }
+}
---
+++ app/src/main/java/com/example/gzhang/stockscreener/LogInActivity.java
@@ -0,0 +1,67 @@
+package com.example.gzhang.stockscreener;
+
+import android.app.Activity;
+
+import android.content.Intent;
+import android.graphics.BitmapFactory;
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+import android.widget.CheckBox;
+
+/*
+import com.amazonaws.ClientConfiguration;
+import com.amazonaws.auth.CognitoCachingCredentialsProvider;
+import com.amazonaws.regions.Region;
+import com.amazonaws.regions.Regions;
+import com.amazonaws.services.dynamodbv2.*;
+import com.amazonaws.mobileconnectors.dynamodbv2.dynamodbmapper.*;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Scanner;
+*/
+
+/**
+ * Created by leon_ on 2017-06-29.
+ */
+
+public class LogInActivity extends Activity{
+
+    Button loginButton,
+            createaccountButton;
+
+    CheckBox rememberBox;
+
+    String usernameText,
+            passwordText;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+            setContentView(R.layout.login_layout);
+
+
+        loginButton = (Button) findViewById(R.id.loginButton);
+        createaccountButton = (Button) findViewById(R.id.createaccountButton);
+        rememberBox = (CheckBox) findViewById(R.id.rememberBox);
+
+    }
+
+    protected void onAccountClick (View view){
+        Intent intent = new Intent (this, CreateAccountActivity.class);
+        startActivity (intent);
+
+    }
+}
